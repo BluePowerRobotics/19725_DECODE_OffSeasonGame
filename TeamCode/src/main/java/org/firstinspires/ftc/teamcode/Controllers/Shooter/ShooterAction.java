468cb0ded11723fd2e2a0e2f2fa40837292b3bfe
zihengtangtzh25 <kexx35b24@126.com>
2026-01-26 00:15:54 +0800

    Refactor ShooterAction to use single shooter instance
    Replaces dual shooter logic with a single Shooter instance in ShooterAction. Simplifies methods and telemetry to reflect the change, removing left/right distinctions and related filters.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Shooter/ShooterAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Shooter/ShooterAction.java
@@ -10,56 +10,43 @@ public class ShooterAction {
     MeanFilter FilterLeft;
     MeanFilter FilterRight;
     private Telemetry telemetry;
-    private Shooter shooter_Left;
-    private Shooter shooter_Right;
+    private Shooter shooter;
 
     private boolean whetherReachTargetSpeed = false;
 
-    public static int WindowSize = 3;
+    public int WindowSize = 3;
     public ShooterAction(HardwareMap hardwareMap, Telemetry telerc) {
         telemetry = telerc;
         FilterLeft = new MeanFilter(WindowSize);
-        FilterRight = new MeanFilter(WindowSize);
-        shooter_Left = new Shooter(hardwareMap, telemetry, "shooterMotor1", true);
-        shooter_Right = new Shooter(hardwareMap, telemetry, "shooterMotor2", false);
+        shooter = new Shooter(hardwareMap, telemetry, "shooterMotor", true);
 
     }
     public boolean setShootSpeed(int TargetSpeedRC){
-        boolean left;
-        shooter_Left.shoot(TargetSpeedRC);
-        boolean right;
-        shooter_Right.shoot(TargetSpeedRC);
-        FilterLeft.filter(shooter_Left.getCurrent_speed());
-        FilterRight.filter(shooter_Right.getCurrent_speed());
-        left = (Math.abs(FilterLeft.getMean() - TargetSpeedRC) < Shooter.SpeedTolerance);
-        right = (Math.abs(FilterRight.getMean() - TargetSpeedRC) < Shooter.SpeedTolerance);
-        whetherReachTargetSpeed = left && right;
-        //todo 检查这里所有&&和||的逻辑
-        return (left && right);
+        boolean shootersituation;
+        shooter.shoot(TargetSpeedRC);
+        FilterLeft.filter(shooter.getCurrent_speed());
+        shootersituation = (Math.abs(FilterLeft.getMean() - TargetSpeedRC) < Shooter.SpeedTolerance);
+        whetherReachTargetSpeed = shootersituation;
+        return (shootersituation);
+
 
     }
     public void setTelemetry(){
         telemetry.addData("Whether Reach Target Speed", this.whetherReachTargetSpeed());
-        telemetry.addData("Shooter Power Left", this.getPowerLeft());
-        telemetry.addData("Shooter Power Right", this.getPowerRight());
-        telemetry.addData("Shooter Speed Left", this.getCurrent_speed_Left());
-        telemetry.addData("Shooter Speed Right", this.getCurrent_speed_Right());
-
+        telemetry.addData("Shooter Power ", this.getPower());
+        telemetry.addData("Shooter Speed ", this.getSpeed());
     }
     public boolean whetherReachTargetSpeed(){
         return whetherReachTargetSpeed;
     }
-    public double getPowerLeft(){
-        return shooter_Left.Power;
+    public double getPower(){
+        return shooter.Power;
     }
-    public double getPowerRight(){
-        return shooter_Right.Power;
+    public double getSpeed(){return shooter.getCurrent_speed();}
+
     }
 
-    public double getCurrent_speed_Left(){
-        return shooter_Left.current_speed;
-    }
-    public double getCurrent_speed_Right(){
-        return shooter_Right.current_speed;
-    }
-}
+
+
+
+
