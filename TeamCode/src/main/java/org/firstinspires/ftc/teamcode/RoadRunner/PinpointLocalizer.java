f3e0e9c714f3c1d8bfb7015e60c8c761876dd0f4
zihengtangtzh25 <kexx35b24@126.com>
2026-07-20 01:32:40 +0800

    Update PinpointLocalizer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/PinpointLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/PinpointLocalizer.java
@@ -25,10 +25,11 @@ public final class PinpointLocalizer implements Localizer {
      * Pinpoint 定位器的参数配置类
      */
     public static class Params {
-        public double parYTicks = 0.0; // 平行编码器的 y 位置（tick 单位）
-        public double perpXTicks = 0.0; // 垂直编码器的 x 位置（tick 单位）
-        public double pinXOffset = 0.0; // Pinpoint 基准点相对于机器人中心的 X 偏移（英寸，+为前方）
-        public double pinYOffset = 0.0; // Pinpoint 基准点相对于机器人中心的 Y 偏移（英寸，+为左侧）
+        public double parYTicks =  -2460.9120957820664; // 平行编码器的 y 位置（tick 单位）
+        public double perpXTicks = 1907.1003263946116; // 垂直编码器的 x 位置（tick 单位）
+        public double pinXOffset = 1907.1003263946116*0.001999; // Pinpoint 基准点相对于机器人中心的 X 偏移（英寸，+为前方）
+        public double pinYOffset = -2460.9120957820664*0.001999; //
+        // Pinpoint 基准点相对于机器人中心的 Y 偏移（英寸，+为左侧）
     }
 
     /**
