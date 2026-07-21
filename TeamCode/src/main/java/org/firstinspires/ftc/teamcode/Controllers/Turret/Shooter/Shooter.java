53b595bbca1b92a382c23be45607f88f2bd80114
Lee <110874909+TerryLee667@users.noreply.github.com>
2026-07-21 20:29:19 +0800

    大修4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Turret/Shooter/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Turret/Shooter/Shooter.java
@@ -18,11 +18,11 @@ public class Shooter {
     /** 积分系数 */
     public static double kI = 0;
     /** 微分系数 */
-    public static double kD = 0.00000001;
+    public static double kD = 0;
     /** 积分上限 */
     public static double maxI = 1.0;
     /** 静态摩擦系数 */
-    public static double kS = 1.2303;
+    public static double kS = 0.8;
     /** 速度系数 */
     public static double kV = 0.0049;
     /** 加速度系数 */
